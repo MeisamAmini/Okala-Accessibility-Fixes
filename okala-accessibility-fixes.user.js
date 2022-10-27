107c5935af9640d650653f57d93837275f8fd66a
Meisam Amini <meisamamini21@gmail.com>
2022-10-27 22:25:43 +0330

    First commit

---
+++ okala-accessibility-fixes.user.js
@@ -0,0 +1,43 @@
+// ==UserScript==
+// @name         Okala Accessibility Fixes
+// @version      0.1
+// @description  Fixes some problems in the Okala website.
+// @author       Meisam Amini
+// @match        https://okala.com/*
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+// The website uses Ajax to change pages. Repeat the script every 1 second so the fixes are done for all pages.
+setInterval(function() {
+// Fix the product titles which are heading level 3 and link.
+let links = document.querySelectorAll("h3 > a[aria-label]");
+
+if (links) {
+links.forEach(function(link){
+link.setAttribute("aria-label", link.textContent);
+});
+}
+
+// Fixes the titles which are links and are situated above every heading level 3 title.
+links = document.querySelectorAll("div.imageContainer > a[aria-label]");
+
+if (links) {
+links.forEach(function(link){
+const image = link.querySelector("div > img");
+if (image) {link.setAttribute("aria-label", image.alt);}
+});
+}
+
+// Fix the label of add to cart buttons.
+const buttons = document.querySelectorAll("button[aria-label='add to cart']");
+
+if (buttons) {
+buttons.forEach(function(button) {
+button.setAttribute("aria-label", "اضافه‌کردن به سبد خرید");
+});
+}
+}, 1000);
+})();
---
+++ readme.md
@@ -0,0 +1,15 @@
+# Okala Accessibility Fixes | اصلاحات دسترس‌پذیری برای سایت Okala #
+
+## کاربرد ##
+
+[Okala](https://okala.com) یک سوپرمارکت آنلاین است که در شهرهای مختلف ایران خدمات ارائه می‌دهد. این وبسایت چند مشکل دسترس‌پذیری دارد که کار با آن را برای افراد نابینا دشوار می‌کند.
+
+این userscript که برای افزونه‌هایی مثل TamperMonkey نوشته شده است، برخی از این مشکلات را رفع می‌کند. تا جایی که افراد نابینا می‌توانند از این وبسایت استفاده کنند.
+
+## نصب ##
+
+افزونه‌هایی مثل **TamperMonkey** وجود دارند که به کاربران اجازه می‌دهند برای وبسایت‌های مختلف اسکریپت‌های کوچکی به نام userscript بنویسند. این اسکریپت‌ها در هنگام بازشدن آدرس مناسب فعال می‌شوند و می‌توانند تغییراتی را در صفحه ایجاد کنند. این موضوع باعث می‌شود این افزونه‌ها برای اصلاح برخی از مشکلات دسترس‌پذیری مناسب باشند. برای استفاده از این userscript مراحل زیر را دنبال کنید.
+
+اول افزونه **TamperMonkey** را برای Google Chrome از [اینجا](https://chrome.google.com/webstore/detail/tampermonkey/dhdgffkkebhmkfjojejmpbldmpobfkfo?hl=en) و برای Firefox از [اینجا](https://addons.mozilla.org/en-US/firefox/addon/tampermonkey/) نصب کنید.
+
+سپس آخرین نسخه این userscript را از [اینجا](https://github.com/MeisamAmini/wikipedia-link-remover/raw/main/wikipedia-link-remover.user.js) نصب کنید.
